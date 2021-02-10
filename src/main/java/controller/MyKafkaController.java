1434c1e05df0ead9378348d22e24948e7741fa18
g <MDf4nBNjqrufGnxS@protonmail.com>
2021-02-10 19:24:11 +0300

    Instance create

--- src/main/java/controller/MyKafkaController.java
+++ src/main/java/controller/MyKafkaController.java
@@ -4,9 +4,9 @@ import workers.KafkaMyExample;
 
 public class MyKafkaController {
 
-    private static String username = "";
-    private static String password = "";
-    private static String brokers = "";
+    private static String username = "1hg9gmta";
+    private static String password = "GKgmM21mW3nSz_THBTvHRS5hOfXBTJy5";
+    private static String brokers = "tricycle.srvs.cloudkafka.com";
 
     public static void main(String[] args) {
 
